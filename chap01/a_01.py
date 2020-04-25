1da496ea616d93db91192e0c7e59d3315836b7b2
tardigrades <dothisnows@gmail.com>
2020-04-25 21:00:10 +0900

    Assignment 02

---
+++ chap01/a_01.py
@@ -0,0 +1,4 @@
+def get_odd_positions(str):
+    if len(str) < 2:
+        return ""
+    return str[1::2]
--- chap01/test.py
+++ chap01/test.py
@@ -1,9 +1,18 @@
 import pytest
 
 import a_00
+import a_01
 
 def test_reverse_string_function():
     assert a_00.reverse_string("stressed") == "desserts"
     assert a_00.reverse_string("abc") == "cba"
     assert a_00.reverse_string("a") == "a"
     assert a_00.reverse_string("123456789") == "987654321"
+    assert a_00.reverse_string("あカ美人") == "人美カあ"
+
+def test_get_odd_positions():
+    assert a_01.get_odd_positions("パタトクカシーー") == "タクシー"
+    assert a_01.get_odd_positions("0123456789") == "13579"
+    assert a_01.get_odd_positions("abcdef") == "bdf"
+    assert a_01.get_odd_positions("a") == ""
+    assert a_01.get_odd_positions("") == ""
